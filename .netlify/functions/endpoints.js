cb5fb90c798ca5fd546226177a209e2ff1a2fa2d
ullasm <ullasm@gmail.com>
2022-11-06 14:51:00 +0530

    netlify config changes to check api issue

--- .netlify/functions/endpoints.js
+++ .netlify/functions/endpoints.js
@@ -1,8 +1,9 @@
-exports.handler = async (event, context) => {
+exports.handler= async(event, context) =>  {
+    console.log("In netlfiy",process.env.REACT_APP_API_KEY)
     return {
         statusCode: 200,
         body: JSON.stringify({
         api: process.env.REACT_APP_API_KEY
-        }),
+        })
     }
 }
